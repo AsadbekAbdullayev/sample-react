534a7efcd7009b5607813f71d683a495a1142db2
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-02-14 23:39:48 +0500

    Update Loader.jsx

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -3,6 +3,7 @@ import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 import { useNavigate, useLocation } from 'react-router-dom';
 
+
 export const Loader = ({ size }) => {
 	const { pathname } = useLocation();
 	return (
